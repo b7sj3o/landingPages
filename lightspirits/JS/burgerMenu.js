922f86e33ce36f37572397c1c4e4407a6bc69fc4
b7sj3o <vitalikfedytnyk@gmail.com>
2024-05-10 23:40:05 +0300

    lightspirits_v1

--- lightspirits/JS/burgerMenu.js
+++ lightspirits/JS/burgerMenu.js
@@ -1,4 +1,4 @@
-$('.header__burger, .overlay').click(function(){
+$('.header__burger, .overlay, .header__mobile__nav_link').click(function(){
     $('.header__burger').toggleClass('clicked');
     $('.overlay').toggleClass('show');
     $('.header__mobile').toggleClass('show');
