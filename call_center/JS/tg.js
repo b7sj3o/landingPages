6b83d0e7eac7a4b2b07ed87a8878bbeb2199ffe7
Vitaliy Fedytnyk <vitalikfedytnyk@gmail.com>
2025-03-31 19:42:20 +0300

    forms

---
+++ call_center/JS/tg.js
@@ -0,0 +1,31 @@
+const bot_token = '6406654898:AAH01hMh8y8CqJP55D6XtLUYOan7DQh-KeU'
+const chat_id = '928132950'
+
+const form = document.getElementById(`form`);
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    const name = form.elements['name'].value
+    const phone = form.elements['phone'].value
+    const username = form.elements['username'].value
+
+    const messageToBot = `
+Заявка з сайту:%0A
+Ім'я: ${name}%0A
+Номер телефону: ${phone}%0A
+Нікнейм: ${username}
+`
+
+    const url = `https://api.telegram.org/bot${bot_token}/sendMessage?chat_id=${chat_id}&text=${messageToBot}`;
+
+    fetch(url, {
+        method: 'POST',
+      })
+    .then(() => {
+        alert("Дякуємо! Ваше повідомлення надіслано.");
+        form.reset(); // очищаємо форму після надсилання
+    })
+    .catch((error) => {
+        console.error('Помилка при надсиланні даних:', error);
+    });
+});
